bccad36372c03b14845a6e148968cd6a77179ee2
王小天 <wxt1994@gmail.com>
2014-09-11 19:46:46 -0700

    Completed all of the functions. Margin need to be modified.

--- GridView/XWViewController.m
+++ GridView/XWViewController.m
@@ -8,7 +8,9 @@
 
 #import "XWViewController.h"
 
-@interface XWViewController ()
+@interface XWViewController () {
+  UIView* _gridView;
+}
 
 @end
 
@@ -17,13 +19,72 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+  self.view.backgroundColor = [UIColor whiteColor];
+  
+  // create grid frame
+  CGRect frame = self.view.frame;
+  CGFloat x = CGRectGetWidth(frame)*0.1;
+  CGFloat y = CGRectGetHeight(frame)*0.1;
+  CGFloat size = MIN(CGRectGetWidth(frame), CGRectGetHeight(frame))*0.80;
+  
+  CGRect gridFrame = CGRectMake(x, y, size, size);
+  
+  // create grid view
+  _gridView = [[UIView alloc] initWithFrame:gridFrame];
+  _gridView.backgroundColor = [UIColor blackColor];
+  [self.view addSubview:_gridView];
+  
+  // create button
+  CGFloat buttonSize = size / 12.0;
+  CGFloat margin = (size - 9*buttonSize) / 10.0;
+  
+  
+  
+  for (int i = 0; i < 81; i++) {
+    // row and col
+    int col = i % 9;
+    int row = i / 9;
+    
+    // relative pos
+    CGFloat a = x + margin + col * (buttonSize + margin);
+    CGFloat b = y + margin + row * (buttonSize + margin);
+    
+    // add button
+    UIButton *one = [[UIButton alloc] init];
+    one.frame = CGRectMake(a, b, buttonSize, buttonSize);
+    one.backgroundColor = [UIColor whiteColor];
+    one.tag = i;
+    [self.view addSubview:one];
+    
+    // [self addButton:buttonSize x:a y:b btag:i];
+    
+    // create target for button
+    
+    [one addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+  }
+  
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+//-(void)addButton:(CGFloat)size x:(CGFloat)x y:(CGFloat)y btag:(int)tag
+//{
+//  UIButton *one = [[UIButton alloc] init];
+//  one.frame = CGRectMake(x, y, size, size);
+//  one.backgroundColor = [UIColor whiteColor];
+//  one.tag = tag;
+//  [self.view addSubview:one];
+//}
+
+- (void)buttonPressed:(id)sender
+{
+  UIButton *button = (UIButton *)sender;
+  NSLog(@"You pressed the button with tage %d", button.tag);
 }
 
 @end
